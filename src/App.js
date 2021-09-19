3bb5e93102dc69e5fc088cf18b3eacb6deb6dc92
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-09-19 23:07:18 +0600

    add test Countres api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counteries></Counteries>
     </div>
   );
 }
 
+
+function Counteries() {
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+  }, [])
+
+  return (
+    <div>
+      <h1>Trivel in World without Tikits </h1>
+      <h3>View Countries: {countries.length}</h3>
+    </div>
+  )
+}
 export default App;
