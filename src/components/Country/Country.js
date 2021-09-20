a4b7a91e8b4958c0009d3b9232b28b796305f575
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-09-20 08:20:33 +0600

    Load Data in api add add country componet

---
+++ src/components/Country/Country.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <div>
+            <h2>Hi Vist The my own Country</h2>
+        </div>
+    );
+};
+
+export default Country;
--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Country from '../Country/Country';
 import './Countries.css'
 
 const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() => {
+        fetch("https://restcountries.eu/rest/v2/all")
+            .then(res => res.json())
+            .then(data => setCountries(data));
+    }, [])
+
     return (
         <div className="country">
-            <h1>Hi I tall my won file 🧑🏼🧑🏼👨🏼👩🏼‍🎨!!</h1>
+            <h1>Hi I tall my won file 🧑🏼🧑🏼{countries.length}👨🏼👩🏼‍🎨!!</h1>
+            <Country></Country>
         </div>
     );
 };
