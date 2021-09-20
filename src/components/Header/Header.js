e8001aeb1b44037c2bc4b2c175c657b7761a53ef
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-09-20 08:11:21 +0600

    make component to Extanal folder

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Hi🤹🏼‍♀️🤽🏼‍♂️ I am Header👨🏼‍🏫</h1>
+        </div>
+    );
+};
+
+export default Header;
